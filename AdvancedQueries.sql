a458223ce3947706ab289d599a1c552e47ba21c6
Matthew Fong <matthewykfong@gmail.com>
2021-11-02 17:29:26 -0400

    added something significant

--- AdvancedQueries.sql
+++ AdvancedQueries.sql
@@ -17,6 +17,7 @@ HAVING AVG(amount_paid) > (
         )
 );
 
+
 -- Provide a list of the IP Item names and associated total copies sold to all buyers, sorted from the IP Item that has sold the most individual copies to the IP Item that has sold the least.
 
 -- Provide a list of the IP Item names and associated dollar totals for copies sold to all buyers, sorted from the IP Item that has sold the highest dollar amount to the IP Item that has sold the smallest.
